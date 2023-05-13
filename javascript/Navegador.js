7eb4063f32172bb772ccdee26ec9a2b1257bd196
BlueCario123 <44036391+BlueCario123@users.noreply.github.com>
2023-05-13 18:48:01 +0200

    Search Bar implementada en Comentarios

--- javascript/Navegador.js
+++ javascript/Navegador.js
@@ -17,3 +17,37 @@ window.addEventListener('resize', function() {
     document.getElementById('boton-campana').style.display = 'block';
   }
 });
+
+function buscar()
+{
+
+	var id = "searchbar";
+	var barra = document.getElementById(id);
+	var busqueda = barra.value;
+  if (busqueda != "")
+  {
+	  console.log("buscando " + busqueda);
+	  var elem = busquedaRecursiva(document.body, busqueda);
+	  elem.scrollIntoView();
+  }
+}
+
+function busquedaRecursiva(elem, string)
+{
+	if (elem.innerHTML.includes(string))
+		return elem;
+	else if (elem.hasChildNodes())
+	{
+		var res = null;
+		for (var i = 0; i<elem.children.length && res == null;i++)
+		{
+			var child = elem.children[i];
+			res = busquedaRecursiva(child, string);
+		}
+		return res;
+	}
+	else
+	{
+		return null;
+	}
+}
