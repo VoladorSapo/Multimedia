38ef601dba5b95fc19a7ee31e2638dbfa029d24c
VoladorSapo <voladorsapo@gmail.com>
2023-05-13 02:38:46 +0200

    Xd

---
+++ javascript/donar.js
@@ -0,0 +1,6 @@
+var intro = document.getElementById('intro')
+var prize = 	 sessionStorage.getItem("prize");
+var tier = sessionStorage.getItem("tier");
+intro.innerHTML = `<p> Bienvenido a la página de contribuciones. <br> 
+Para terminar tu contribucion con la tier  ${tier} manda un bizum al número 689327980 con al menos   ${prize} € y tu correo electrónico.
+</p>`
---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+function changescene(tier,prize){
+	console.log(tier)
+		sessionStorage.setItem("tier", tier);
+		sessionStorage.setItem("prize", prize);
+
+}
