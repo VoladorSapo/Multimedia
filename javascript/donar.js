4a328788903f0f706f4f9576beac889256bfd445
Andyy728 <andrealb0112@gmail.com>
2023-05-14 12:17:07 +0200

    GEORGIA

--- javascript/donar.js
+++ javascript/donar.js
@@ -4,3 +4,10 @@ var tier = sessionStorage.getItem("tier");
 intro.innerHTML = `<h3> Bienvenido a la página de contribuciones. <br> 
 Para terminar tu contribucion con la tier  ${tier} manda un bizum al número 689327980 con al menos   ${prize} € y tu correo electrónico.
 </h3>`;
+
+/*ESTILO*/
+intro.style.color = "#644400"; 
+
+intro.style.fontSize = "90%";
+
+intro.style.fontFamily = "Georgia";
