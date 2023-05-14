ddc76d97f5b9d0471ba695e13f7e540deff71434
VoladorSapo <voladorsapo@gmail.com>
2023-05-14 23:30:10 +0200

    LoquitAS

--- javascript/donar.js
+++ javascript/donar.js
@@ -1,7 +1,7 @@
 var intro = document.getElementById('intro')
 var prize = 	 sessionStorage.getItem("prize");
 var tier = sessionStorage.getItem("tier");
-intro.innerHTML = `<h3> Bienvenido a la página de contribuciones. <br> 
+intro.innerHTML = `<br><h3> Bienvenido a la página de contribuciones. <br> 
 Para terminar tu contribucion con la tier  ${tier} manda un bizum al número 689327980 con al menos   ${prize} € y tu correo electrónico.
 </h3>`;
 
